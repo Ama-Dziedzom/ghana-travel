108652491f06ec3c0ab7f4755b3e41195f5e8643
Ama-Dziedzom <dziedzom.barnor@gmail.com>
2026-03-30 16:21:51 +0000

    add contentlayer config

---
+++ contentlayer.config.ts
@@ -0,0 +1,25 @@
+// contentlayer.config.ts
+import { defineDocumentType, makeSource } from 'contentlayer2/source-files'
+
+export const Post = defineDocumentType(() => ({
+  name: 'Post',
+  filePathPattern: `**/*.mdx`,
+  contentType: 'mdx',
+  fields: {
+    title: { type: 'string', required: true },
+    date: { type: 'date', required: true },
+    description: { type: 'string', required: false },
+    image: { type: 'string', required: false },
+  },
+  computedFields: {
+    slug: {
+      type: 'string',
+      resolve: (post) => post._raw.flattenedPath,
+    },
+  },
+}))
+
+export default makeSource({
+  contentDirPath: 'content',
+  documentTypes: [Post],
+})
